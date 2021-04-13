2b9f51c22bef61a066d2285544d5b1b9cf7f0cac
FoolerGraham <hayhayking@outlook.com>
2021-04-13 02:58:31 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const app = express();
+
+const mockUserData=[
+    {name:'Mark'},
+    {name:'Jill'}
+]
+app.get('/users', functtion(req,res){
+        res.json({
+            success: true,
+            message: 'successfullygot users. Nice!',
+            users: mockUserData
+        })
+})
+
 app.listen(8000,function(){
     console.log("server is runnig")
 })
